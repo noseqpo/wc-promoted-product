5849c314da252bd245907a9c6aad1ed7cf0c8798
Pax\Daniel <danielnicolaspeace@gmail.com>
2023-06-21 19:57:35 -0300

    minor themes support updates

--- js/positions.js
+++ js/positions.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
     var myDiv = document.getElementById('banner-ppd');
     var siteh = document.getElementById('site-header');
+    var mainh = document.getElementById('main-header');
     var masthead = document.getElementById('masthead');
     var header = document.getElementById('header');
 
-    if (myDiv && (siteh || masthead)) {
+    if (myDiv && (siteh || mainh || masthead || header)) {
         if (siteh) {
             siteh.appendChild(myDiv);
+        } else if (mainh) {
+            mainh.appendChild(myDiv);
         } else if (masthead) {
             masthead.appendChild(myDiv);
         } else if (header) {
